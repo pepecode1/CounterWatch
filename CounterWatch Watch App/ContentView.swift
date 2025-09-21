79f4d3beb8605bb7ecc0aa2e617915e25400041b
Antonio Ruiz <pepecode1@gmail.com>
2025-09-21 10:22:26 -0600

    Initial Commit

---
+++ CounterWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CounterWatch Watch App
+//
+//  Created by Pepe Ruiz on 21/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CounterWatch Watch App/CounterWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterWatchApp.swift
+//  CounterWatch Watch App
+//
+//  Created by Pepe Ruiz on 21/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CounterWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
